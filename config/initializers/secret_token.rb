edd3b7af02d47ee74dbb9e0116954ec48db93fe4
Kevin Trotter <cskevint@gmail.com>
2013-02-09 16:17:05 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Youthconf::Application.config.secret_token = '775d94a846e1cf67da70a03bc87c79563250db27f7090e84527b0a746a7eeb4d491f01e4cdb1858ff04de1789addc891c5c4e4dff36653486b57542566f97511'
