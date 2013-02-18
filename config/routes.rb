f9cc0bab7b114d7942cdec021f9eb4b8c42c4037
Kevin Trotter <cskevint@gmail.com>
2013-02-17 20:16:11 -0800

    initial post scaffolding

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Youthconf::Application.routes.draw do
 
+  resources :posts
+
+
   resources :users
 
   resources :conferences do
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130217065501) do
+ActiveRecord::Schema.define(:version => 20130218041305) do
 
   create_table "conferences", :force => true do |t|
     t.string   "city"
@@ -29,6 +29,14 @@ ActiveRecord::Schema.define(:version => 20130217065501) do
   create_table "conferences_users", :id => false, :force => true do |t|
     t.integer "conference_id"
     t.integer "user_id"
+  end
+
+  create_table "posts", :force => true do |t|
+    t.string   "name"
+    t.text     "content"
+    t.boolean  "promoted"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "sessions", :force => true do |t|
